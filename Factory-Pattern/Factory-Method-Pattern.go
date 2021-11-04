a1a8552b9a1ec9de2223d77b8244c9d3bc519cc9
JakeLin <jakelin0fly@gmail.com>
2021-11-04 15:02:43 +0800

    工厂方法模式

---
+++ Factory-Pattern/Factory-Method-Pattern.go
@@ -0,0 +1,39 @@
+package Factory_Pattern
+
+//// 抽象产品
+//type IShape interface {
+//	Draw()
+//}
+//
+//// 具体产品：圆形
+//type Circle struct { }
+//
+//func (p *Circle) Draw() {
+//	fmt.Println("draw a circle")
+//}
+//
+//// 具体产品：三角形
+//type Rectangle struct { }
+//
+//func (p *Rectangle) Draw() {
+//	fmt.Println("draw a rectangle")
+//}
+
+// 抽象工厂
+type IFactory interface {
+	CreateShape()
+}
+
+// 具体工厂：圆形工厂
+type CircleFactory struct {}
+
+func (p *CircleFactory) CreateShape() IShape {
+	return &Circle{}
+}
+
+// 具体工厂：三角形工厂
+type RectangleFactory struct {}
+
+func (p *RectangleFactory) CreateShape() IShape {
+	return &Rectangle{}
+}
--- Factory-Pattern/Simple-Factory-Pattern.go
+++ Factory-Pattern/Simple-Factory-Pattern.go
@@ -6,33 +6,33 @@ import (
 )
 
 // 抽象产品
-type Shape interface {
-	draw()
+type IShape interface {
+	Draw()
 }
 
 // 具体产品：圆形
 type Circle struct { }
 
-func (p *Circle) draw() {
+func (p *Circle) Draw() {
 	fmt.Println("draw a circle")
 }
 
 // 具体产品：三角形
 type Rectangle struct { }
 
-func (p *Rectangle) draw() {
+func (p *Rectangle) Draw() {
 	fmt.Println("draw a rectangle")
 }
 
 // 工厂
 type Factory struct { }
 
-func (p *Factory) create(name string) (Shape, error) {
+func (p *Factory) create(name string) (IShape, error) {
 	switch name {
 	case "circle":
 		return &Circle{}, nil
 	case "rectangle":
 		return &Rectangle{}, nil
 	}
-	return nil, errors.New("unsupported name")
+	return nil, errors.New("unsupported shape")
 }
